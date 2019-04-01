180fbf0d6d70d90b1c777585baaaffed5c9fff1b
Sorin Baptiste <baptiste.sorin@insa-lyon.fr>
2019-04-01 18:01:37 +0200

    header file of class String
    declaration of the parameters and default constructor of the class String

---
+++ String.h
@@ -0,0 +1,29 @@
+#include<cstddef>
+
+#ifndef STRING_ 
+#define STRING_
+
+class String{
+  public :
+  //Constructors
+  String();
+  //getters
+  
+  //setters
+  
+  //methods
+  
+  protected : 
+  //attributes
+  static size_t MAX_SIZE_;
+  size_t size_;
+  size_t capacity_;
+  char* str_;
+  
+  
+    
+    
+
+
+};
+#endif //STRING_
