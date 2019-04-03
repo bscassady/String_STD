b841398854fc6e711ef729d8ac9d4e04ebda7671
Titouan <tpoquillon@gmail.com>
2019-04-03 13:30:46 +0200

    bug_fix #3
    size know include the '\0' ending char
    
    Signed-off-by: Titouan <tpoquillon@gmail.com>

--- String.cpp
+++ String.cpp
@@ -5,7 +5,7 @@ size_t String::MAX_SIZE_=100;
 
 String::String(size_t Arbitrary_value_of_capacity){
   capacity_=Arbitrary_value_of_capacity;
-  size_ = 4;
+  size_ = 5;
   str_=new char[capacity_];
   str_[0]='T';
   str_[1]='o';
