5d033a5d4df9d818a3c1eff1047eab52e7dab054
Titouan <tpoquillon@gmail.com>
2019-04-05 18:10:12 +0200

    Limitation of Size_ =with MAX_SIZE_
    Add a limitation in size that can be taken by the string object
    
    Signed-off-by: Titouan <tpoquillon@gmail.com>

--- String.cpp
+++ String.cpp
@@ -19,11 +19,15 @@ String::String(const char* s){
   while(s[size_]!='\0'){
     ++size_;
   }
+  if(size_>MAX_SIZE_){
+  size_=MAX_SIZE_;
+  }
   capacity_ = size_;
   str_ = new char[capacity_+1];
-  for(unsigned int i =0;i<=size_;++i){
+  for(unsigned int i =0;i<size_;++i){
     str_[i]=s[i];
   }
+  str_[size_]='\0'; 
 }
 
 String::~String(){
@@ -31,12 +35,16 @@ String::~String(){
 }
 
 void String::reserve (size_t n){
+  if(n>MAX_SIZE_){
+  n=MAX_SIZE_;
+  }
   if (capacity_< n){
     char* strbis=str_;
     str_=new char[n+1];
-    for (size_t i=0;i<=size_;++i){
+    for (size_t i=0;i<size_;++i){
       str_[i]=strbis[i];    
     }
+    str_[size_]='\0'; 
     delete strbis;
     capacity_=n;    
   }
@@ -68,13 +76,17 @@ String& String::operator= (const char* s){
   while(s[size_]!='\0'){
     ++size_;
   }
+  if(size_>MAX_SIZE_){
+  size_=MAX_SIZE_;
+  }
   if (capacity_<size_){
     capacity_=size_;
     str_=new char[capacity_+1];
   }
-  for(unsigned int i =0;i<=size_;++i){
+  for(unsigned int i =0;i<size_;++i){
     str_[i]=s[i];
-  }  
+  }
+  str_[size_]='\0'; 
   
 
   return *this;
@@ -83,15 +95,19 @@ String& String::operator= (const char* s){
 String operator+ (const String& lhs, const String& rhs){
   String* str= new String("");
   str->size_=lhs.size_+rhs.size_;
+  if(str->size_>str->MAX_SIZE_){
+  str->size_=str->MAX_SIZE_;
+  }
   str->capacity_=lhs.size_+rhs.size_;
   delete str->str_;
   str->str_=new char[str->capacity_+1];
   for(unsigned int i =0;i<lhs.size_;++i){
     str->str_[i]=lhs.str_[i];
   }
-  for(unsigned int i =0;i<=rhs.size_;++i){
-    str->str_[i+lhs.size_]=rhs.str_[i];
+  for(unsigned int i =lhs.size_;i<str->size_;++i){
+    str->str_[i]=rhs.str_[i-lhs.size_];
   }
+  str->str_[str->size_]='\0';
   return *str;
   
   
--- main.cpp
+++ main.cpp
@@ -27,6 +27,7 @@ std::cout<<(test1->length()==8)<<std::endl;
 
 //Test operator+(String)
 String test3="baudruche";
+
 String test4=test3+*test1;
 std::cout<<(test4.length()==17)<< (test4.capacity()==17) <<std::endl;
 
@@ -34,10 +35,21 @@ std::cout<<(test4.length()==17)<< (test4.capacity()==17) <<std::endl;
 std::cout<<(test1->max_size()==100)<<std::endl;
 std::cout<<(test2->max_size()==100)<<std::endl;
 
+//Test limitation size
+char c[]       ="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+test3=c;
+std::cout<<(test3.size()==100)<<std::endl;
+test3.reserve(10000);
+std::cout<<(test3.size()==100)<<std::endl;
+String* test5=new String(c);
+std::cout<<(test5->size()==100)<<std::endl;
+String test6 = test3+*test5;
+std::cout<<(test6.size()==100)<<std::endl;
 
 
 
 delete test1;
 delete test2;
+delete test5;
 return 0;
 }
